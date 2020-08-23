782eabb2b192e60b3adacd9ef74a10bfb2a7186a
Ekaterina Pavlova <inthehunterland@gmail.com>
2020-08-23 19:03:59 +0300

    all

--- UI_Test/Assets/Scripts/CrossButton.cs
+++ UI_Test/Assets/Scripts/CrossButton.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
 
 public class CrossButton : MonoBehaviour
 {
    public void ClickCross()
     {
+#if UNITY_EDITOR
         EditorApplication.isPlaying = false;
+#endif
     }
 }
--- UI_Test/Assets/Scripts/Stars.cs
+++ UI_Test/Assets/Scripts/Stars.cs
@@ -11,14 +11,15 @@ public class Stars : MonoBehaviour
 
     void Start()
     {
-        float width = gameObject.transform.position.x;
-        Debug.Log(width);
-        int countStar = Random.Range(1, 5);
+        var rectTrans = gameObject.transform as RectTransform;
+        float width=rectTrans.rect.width;
+
+        int countStar = 3;// Random.Range(1, 5);
         int countGoldStar = Random.Range(0, countStar);
-        Debug.Log(countStar);
+   
         float newX = (width / countStar)/2;
-        width = 200;
-        Debug.Log(gameObject.transform.position);
+        
+
 
         for (float i = 0; i <= countGoldStar-1; i +=1)
         {
@@ -26,28 +27,21 @@ public class Stars : MonoBehaviour
             float newPosX = -width / 2 + newX+ width*i/countStar;
             GameObject newItem = Instantiate(goldStar, transform.position, transform.rotation);
             newItem.name = gameObject.name + i.ToString();
-
-            newItem.transform.parent = gameObject.transform;
+            newItem.transform.SetParent(gameObject.transform);
             newItem.transform.localPosition = new Vector3(newPosX, 0, 0);
 
-            //newX = 2 * newX;
-
-            Debug.Log(newItem.transform.position);
-            Debug.Log(newItem.transform.localPosition); }
+            
+        }
         for (float i = countGoldStar; i <= countStar - 1; i += 1)
         {
 
             float newPosX = -width / 2 + newX + width * i / countStar;
             GameObject newItem = Instantiate(star, transform.position, transform.rotation);
             newItem.name = gameObject.name + i.ToString();
-
-            newItem.transform.parent = gameObject.transform;
+            newItem.transform.SetParent(gameObject.transform);
+            //newItem.transform.parent = gameObject.transform;
             newItem.transform.localPosition = new Vector3(newPosX, 0, 0);
 
-            //newX = 2 * newX;
-
-            Debug.Log(newItem.transform.position);
-            Debug.Log(newItem.transform.localPosition);
         }
     }
 }
